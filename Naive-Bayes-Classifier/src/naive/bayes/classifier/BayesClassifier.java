51549cb14455b096c07d50268839818f668f9ef3
Rahman Adianto <rahman.adianto@gmail.com>
2015-11-19 17:24:00 +0700

    Add Naive-Bayes Project

---
+++ Naive-Bayes-Classifier/src/naive/bayes/classifier/BayesClassifier.java
@@ -0,0 +1,19 @@
+/*
+ * Tugas Besar 2 IF3071 - Intelegensi Buatan
+ */
+package naive.bayes.classifier;
+
+/**
+ *
+ * @author Rahman Adianto
+ */
+public class BayesClassifier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
